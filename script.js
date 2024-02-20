59a31434233e1fe9ac5b6513f64318e10a643bf6
Sandeep Pansari <86551476+sandeep18p@users.noreply.github.com>
2024-02-20 14:17:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-// script.js
-document.addEventListener("DOMContentLoaded", function() {
-    const counterParagraph = document.getElementById("counter");
-    const incrementBtn = document.getElementById("incrementBtn");
-    let counterValue = 0;
+const counterParagraph = document.getElementById("counter");
+const incrementBtn = document.getElementById("incrementBtn");
+let counterValue = 0;
 
-    counterParagraph.textContent = `Counter: ${counterValue}`;
+counterParagraph.textContent = `${counterValue}`;
 
-    incrementBtn.addEventListener("click", function() {
-        alert(`Un-incremented value: ${counterValue}`);
-        counterValue++;
-        counterParagraph.textContent = `Counter: ${counterValue}`;
-    });
+incrementBtn.addEventListener("click", function() {
+    alert(`${counterValue}`);
+    counterValue++;
+    counterParagraph.textContent = `${counterValue}`;
 });
